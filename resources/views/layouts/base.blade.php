9dcccbeb4d06edd231e5e22c3d431958c95151f3
ppolle <peter.m.polle@gmail.com>
2018-06-19 18:00:45 +0300

    this commit will update partials and index page content

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Peter Polle</title>
+    @include('partials._sheets')
+</head>
+<body>
+
+    <!-- Preloader -->
+
+    @include('partials._preloader')  
+
+    <!-- Home start -->
+    @include('partials._home')
+
+    <!-- Home end -->
+    @yield('content')
+
+   
+    <!-- Footer start -->
+
+    @include('partials._footer') 
+
+    <!-- Footer end -->
+
+    <!-- Scroll-up -->
+
+    <div class="scroll-up">
+        <a href="#home"><i class="fa fa-angle-double-up"></i></a>
+    </div>
+
+    <!-- Javascript files -->
+    @include('partials._scripts') 
+
+</body>
+</html>
---
+++ resources/views/partials/_home.blade.php
@@ -0,0 +1,13 @@
+    <section id="home" class="module-image js-height-full">
+
+        <div class="intro">
+            <div class="hello">Hello, my name is Peter Polle and</div>
+            <h1>I am Full-Stack Developer</h1>
+            <div class="who-creates">creating cutting edge web applications for the modern world</div>
+        </div>
+
+        <div class="mouse-icon">
+            <div class="wheel"></div>
+        </div>
+
+    </section>
