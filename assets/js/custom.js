2e1b2243f95f0a6e1b0fd805ef3c4d5dde3778c1
ppolle <peter.m.polle@gmail.com>
2025-04-03 17:48:57 +0300

    this commit will reduce splash screen timer delay

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,7 @@
 
 	$(window).load(function() {
 		$('#status').fadeOut();
-		$('#preloader').delay(50).fadeOut('slow');
+		$('#preloader').delay(5).fadeOut('slow');
 	});
 
 	$(document).ready(function() {
